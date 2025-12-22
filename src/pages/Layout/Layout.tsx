e50d33897ea315dc85607d6700c49bdc647e6764
Anna_German <anna.r.german1410@gmail.com>
2025-12-22 16:14:00 +0300

    feat: update Layout component to display header on all pages

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,25 +1,33 @@
 // General component for all pages (with navigation)
 
-import { Outlet, NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import HeaderToolbar from '@/features/headerToolbar/HeaderToolbar';
+import AsideNavDrawer from '@/features/asideNavDrawer/AsideNavDrawer';
 
 const Layout = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen((prevState) => !prevState);
+  };
+
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/recipes">Recipes</NavLink>
-          </li>
-          <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
-        </ul>
-      </nav>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="header">
+        <HeaderToolbar handleDrawerToggle={handleDrawerToggle} />
+      </AppBar>
+
+      <AsideNavDrawer drawerOpen={drawerOpen} handleDrawerToggle={handleDrawerToggle} />
+
       <Outlet />
-    </>
+    </Box>
   );
 };
 
