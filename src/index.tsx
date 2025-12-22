ba96ce83868eb8ab7253acd38d6822726bf37c93
Anna_German <anna.r.german1410@gmail.com>
2025-12-22 16:00:16 +0300

    feat: add custom theme and ThemeProvider to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '@/app/theme';
+
 import App from '@/App';
 import { store } from '@/app/store';
 
@@ -8,8 +12,10 @@ const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
